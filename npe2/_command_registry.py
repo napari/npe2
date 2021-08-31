0773ae7bc6e6b093cd73318132cabc478a24ea29
Talley Lambert <talley.lambert@gmail.com>
2021-08-31 12:56:08 -0400

    update command registry

--- npe2/_command_registry.py
+++ npe2/_command_registry.py
@@ -4,40 +4,47 @@ from functools import partial
 
 from typing import Callable, Dict, List, NewType, Any, Optional
 
-Command = Callable
-CommandID = str
 PDisposable = Callable[[], None]
+
+from psygnal import Signal
 
 
 class CommandRegistry:
-    _commands: Dict[CommandID, Command] = {}
+    _commands: Dict[str, Callable] = {}
+    commandRegistered = Signal(str)
 
-    # similar to extHostCommands.ts-ExtHostCommands.registerCommand
-    def register_command(self, id: CommandID, command: Command) -> PDisposable:
-        if not id.strip():
-            raise ValueError("invalid id")
+    # similar to:
+    # https://github.com/microsoft/vscode/blob/main/src/vs/platform/commands/common/commands.ts#L61
+    def register_command(self, id: str, command: Callable) -> PDisposable:
+        if not isinstance(id, str) and id.strip():
+            raise ValueError("invalid id, must be string with content")
         if id in self._commands:
             raise ValueError(f"command {id} already exists")
 
+        # TODO: validate argumemnts and type constraints
+        # possibly wrap command in a type validator?
+
         self._commands[id] = command
-        # TODO: emit "did-register" signal
+        self.commandRegistered.emit(id)
+
         return partial(self.unregister_command, id)
 
-    def unregister_command(self, id: CommandID):
+    def unregister_command(self, id: str):
         self._commands.pop(id, None)
 
-    def execute_command(self, id: CommandID, args=(), kwargs={}):
+    def get_command(self, id) -> Callable:
         if id not in self._commands:
             from ._plugin_manager import plugin_manager
 
             if id in plugin_manager._commands:
                 _, plugin_key = plugin_manager._commands[id]
                 plugin_manager.activate(plugin_key)
-
-        try:
-            return self._commands[id](*args, **kwargs)
-        except KeyError:
+        if id not in self._commands:
             raise KeyError(f"command {id!r} not registered")
+        return self._commands[id]
+
+    def execute_command(self, id: str, args=(), kwargs={}):
+        return self.get_command(id)(*args, **kwargs)
 
     def register_command_alias(self, existing_id: str, new_id: str):
         ...
@@ -49,7 +56,7 @@ execute_command = command_registry.execute_command
 # register_command_alias = command_registry.register_command_alias
 
 
-def register_command(cmd_id: str, command: Optional[Command] = None):
+def register_command(cmd_id: str, command: Optional[Callable] = None):
     if command is None:
 
         def register_decorator(cmd):
