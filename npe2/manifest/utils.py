19ba9c50fbbd23b501770e0309e4a955c1d2aa1b
Talley Lambert <talley.lambert@gmail.com>
2021-12-14 11:58:39 -0500

    version object

--- npe2/manifest/utils.py
+++ npe2/manifest/utils.py
@@ -1,6 +1,20 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable, Generic, Optional, TypeVar
+import re
+from dataclasses import dataclass
+from functools import total_ordering
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Optional,
+    SupportsInt,
+    Tuple,
+    TypeVar,
+    Union,
+)
 
 if TYPE_CHECKING:
     from typing_extensions import Protocol
@@ -41,3 +55,85 @@ class Executable(Generic[R]):
     def plugin_name(self: ProvidesCommand):
         # takes advantage of the fact that command always starts with manifest.name
         return self.command.split(".")[0]
+
+
+@total_ordering
+@dataclass
+class Version:
+    """A semver compatible version class.
+
+    mostly vendored from python-semver (BSD-3):
+    https://github.com/python-semver/python-semver/
+    """
+
+    major: SupportsInt
+    minor: SupportsInt = 0
+    patch: SupportsInt = 0
+    prerelease: Union[bytes, str, int, None] = None
+    build: Union[bytes, str, int, None] = None
+
+    _SEMVER_PATTERN = re.compile(
+        r"""
+            ^
+            (?P<major>0|[1-9]\d*)
+            \.
+            (?P<minor>0|[1-9]\d*)
+            \.
+            (?P<patch>0|[1-9]\d*)
+            (?:-(?P<prerelease>
+                (?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)
+                (?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*
+            ))?
+            (?:\+(?P<build>
+                [0-9a-zA-Z-]+
+                (?:\.[0-9a-zA-Z-]+)*
+            ))?
+            $
+        """,
+        re.VERBOSE,
+    )
+
+    @classmethod
+    def parse(cls, version: Union[bytes, str]) -> Version:
+        if isinstance(version, bytes):
+            version = version.decode("UTF-8")
+        match = cls._SEMVER_PATTERN.match(version)
+        if match is None:
+            raise ValueError(f"{version} is not valid SemVer string")
+        matched_version_parts: Dict[str, Any] = match.groupdict()
+        return cls(**matched_version_parts)
+
+    # NOTE: we're only comparing the numeric parts for now.
+    def __eq__(self, other) -> bool:
+        return self.to_tuple()[:3] == self._from_obj(other).to_tuple()[:3]
+
+    def __lt__(self, other) -> bool:
+        return self.to_tuple()[:3] < self._from_obj(other).to_tuple()[:3]
+
+    @classmethod
+    def _from_obj(cls, other):
+        if isinstance(other, (str, bytes)):
+            other = Version.parse(other)
+        elif isinstance(other, dict):
+            other = Version(**other)
+        elif isinstance(other, (tuple, list)):
+            other = Version(*other)
+        elif not isinstance(other, Version):
+            raise TypeError(
+                f"Expected str, bytes, dict, tuple, list, or {cls} instance, "
+                f"but got {type(other)}"
+            )
+        return other
+
+    def to_tuple(self) -> Tuple[int, int, int, Optional[str], Optional[str]]:
+        """Return version as tuple (first three are int, last two Opt[str])."""
+        return (
+            int(self.major),
+            int(self.minor),
+            int(self.patch),
+            str(self.prerelease) if self.prerelease is not None else None,
+            str(self.build) if self.build is not None else None,
+        )
+
+    def __iter__(self):
+        yield from self.to_tuple()
