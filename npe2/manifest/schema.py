000ee494760fbd87c28f7a73c7ff587a76e01da1
Talley Lambert <talley.lambert@gmail.com>
2022-03-13 18:51:31 -0400

    reomve schema change

--- npe2/manifest/schema.py
+++ npe2/manifest/schema.py
@@ -52,7 +52,6 @@ class PluginManifest(ImportExportModel):
         ...,
         description="The name of the plugin. Though this field is mandatory, it *must*"
         " match the package `name` as defined in the python package metadata.",
-        allow_mutation=False,
     )
     _validate_name = validator("name", pre=True, allow_reuse=True)(
         _validators.package_name
@@ -225,7 +224,6 @@ class PluginManifest(ImportExportModel):
     class Config:
         underscore_attrs_are_private = True
         extra = Extra.forbid
-        validate_assignment = True
 
     @classmethod
     def discover(
