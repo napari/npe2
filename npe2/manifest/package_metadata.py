783775eb254d4acd598650e80cc6bf7923f91403
Talley Lambert <talley.lambert@gmail.com>
2022-03-15 08:13:48 -0400

    change message

--- npe2/manifest/package_metadata.py
+++ npe2/manifest/package_metadata.py
@@ -1,7 +1,7 @@
 import warnings
 
 warnings.warn(
-    "please import PackageMetadata from npe2",
+    "Please import PackageMetadata from 'npe2' or from 'npe2.manifest'",
     DeprecationWarning,
     stacklevel=2,
 )
