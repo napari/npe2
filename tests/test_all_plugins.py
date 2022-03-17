c04f7b4b9a69ab29a47c3cab6cf01ddabb493535
Talley Lambert <talley.lambert@gmail.com>
2022-03-17 08:45:03 -0400

    add python_names test

--- tests/test_all_plugins.py
+++ tests/test_all_plugins.py
@@ -1,16 +1,22 @@
 import os
+import warnings
 from typing import List
 
 import pytest
 
-PLUGIN = os.getenv("TEST_PACKAGE_NAME")
-if not PLUGIN:
-    pytest.skip("skipping plugin specific tests", allow_module_level=True)
+from npe2._from_npe1 import _python_name, iter_hookimpls
+from npe2.manifest.utils import SHIM_NAME_PREFIX, import_python_name
 
 try:
     from importlib import metadata
 except ImportError:
     import importlib_metadata as metadata  # type: ignore
+
+
+PLUGIN = os.getenv("TEST_PACKAGE_NAME")
+if not PLUGIN:
+    pytest.skip("skipping plugin specific tests", allow_module_level=True)
+
 
 FORGOT_NAPARI = [
     "vessel-express",
@@ -41,15 +47,39 @@ def test_plugin_has_entry_points(entry_points):
 
 
 if PLUGIN in FORGOT_NAPARI:
-    m = pytest.mark.xfail(reason="forgot napari in deps", strict=True)
+    depsfail = pytest.mark.xfail(reason="forgot napari in deps", strict=True)
 elif PLUGIN == "napari-omero":
-    m = pytest.mark.xfail(reason="needs conda work", strict=True)
+    depsfail = pytest.mark.xfail(reason="needs conda work", strict=True)
 else:
-    m = lambda f: f  # noqa
+    depsfail = lambda f: f  # noqa
 
 
-@m
+@depsfail
 def test_entry_points_importable(entry_points: List[metadata.EntryPoint]):
     for ep in entry_points:
         if ep.group == "napari.plugin":
             ep.load()
+
+
+@depsfail
+def test_npe1_python_names(entry_points: List[metadata.EntryPoint]):
+    for ep in entry_points:
+        if ep.group == "napari.plugin":
+            for impl in iter_hookimpls(ep.load()):
+                if impl.specname not in (
+                    "napari_provide_sample_data",
+                    "napari_experimental_provide_function",
+                    "napari_experimental_provide_dock_widget",
+                ):
+                    continue
+
+                result = impl.function()
+                if isinstance(result, dict):
+                    result = result.values()
+                elif not isinstance(result, list):
+                    result = [result]
+                for idx, item in result:
+                    pyname = _python_name(item, impl.function, idx)
+                    if SHIM_NAME_PREFIX in pyname:
+                        warnings.warn("SHIMMING: %r" % pyname)
+                    assert import_python_name(pyname) is not None
