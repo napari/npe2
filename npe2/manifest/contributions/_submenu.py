1b82cda644447b27bfc37ab8355be695961d3650
Nathan Clack <nclack@chanzuckerberg.com>
2022-05-07 17:30:27 -0700

    Added submenu id validation

--- npe2/manifest/contributions/_submenu.py
+++ npe2/manifest/contributions/_submenu.py
@@ -1,14 +1,16 @@
 from typing import List, Optional, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from pydantic.fields import Field
 
+from .. import _validators
 from ._icon import Icon
 from ._menus import MenuItem
 
 
 class SubmenuContribution(BaseModel):
     id: str = Field(description="Identifier of the menu to display as a submenu.")
+    _valid_id = validator("id", allow_reuse=True)(_validators.command_id)
     label: str = Field(
         description="The label of the menu item which leads to this submenu."
     )
