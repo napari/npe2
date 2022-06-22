e045782d3486d1996155291988b475f82cadf431
Talley Lambert <talley.lambert@gmail.com>
2022-06-22 13:49:24 -0400

    wip playing

--- npe2/_setuptools_plugin.py
+++ npe2/_setuptools_plugin.py
@@ -2,18 +2,17 @@ from __future__ import annotations
 from typing import Any, Callable
 
 from setuptools.command.sdist import sdist
-from setuptools import Command, Distribution
-print("IMPORT")
+import setuptools
+
 
 class compile_npe2(sdist):
     def run(self) -> None:
         breakpoint()
         return super().run()
 
-def npe2_keyword(
-    dist: Distribution,
-    keyword: str,
-    value: bool | dict[str, Any] | Callable[[], dict[str, Any]],
+
+def npe2_compile(
+    dist: setuptools.Distribution,
 ) -> None:
     print(locals())
     breakpoint()
