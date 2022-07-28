3f59ee5aea9e02c3b7f8294d116a55cea7b74e9f
Talley Lambert <talley.lambert@gmail.com>
2022-07-28 10:58:46 -0400

    add get_reader

--- npe2/_inspection/_visitors.py
+++ npe2/_inspection/_visitors.py
@@ -217,6 +217,18 @@ class NPE1PluginModuleVisitor(_DecoratorVisitor):
         except AttributeError:
             print(f"TODO: implement {hookname}")
 
+    def napari_get_reader(self, node: ast.FunctionDef):
+        cmd_id = f"{self.plugin_name}.{node.name}"
+        py_name = f"{self.module_name}:{node.name}"
+        cmd_contrib = contributions.CommandContribution(
+            id=cmd_id, python_name=py_name, title=node.name
+        )
+        rdr_contrib = contributions.ReaderContribution(
+            command=cmd_id, filename_patterns=["*"], accepts_directories=True
+        )
+        self.contribution_points["commands"].append(cmd_contrib)
+        self.contribution_points["readers"].append(rdr_contrib)
+
     def napari_experimental_provide_dock_widget(self, node: ast.FunctionDef):
         return_ = next(n for n in node.body if isinstance(n, ast.Return))
         if isinstance(return_.value, ast.List):
