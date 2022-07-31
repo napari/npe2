d56aa6909cb23cb5070bd0a555961521e6df1a95
Talley Lambert <talley.lambert@gmail.com>
2022-07-31 07:27:17 -0400

    update scope

--- npe2/manifest/contributions/_configuration.py
+++ npe2/manifest/contributions/_configuration.py
@@ -79,7 +79,17 @@ class ConfigurationProperty(Draft07JsonSchema):
         "settings UI unless it is configured by the user.",
     )
 
-    # scope: Literal["window", "machine"] = Field() # TODO: implement this
+    scope: Literal["application"] = Field(
+        "application",
+        description="A configuration setting can have one of the following scopes:\n"
+        "  - `application`: Applies globally, can only be configured in user settings",
+        # TODO: add support for these additional scopes
+        # "  - `machine`: Machine specific settings that can be set only in user "
+        # "settings or only in remote settings. For example, an installation path which"
+        # " shouldn't be shared across machines.\n"
+        # "  - `window`: Viewer (instance) specific settings which can be configured"
+        # "in user or workspace settings.",  # this should be the default
+    )
     edit_presentation: Literal["singleline", "multiline"] = Field(
         "singleline",
         description="A string setting can be rendered with a multiline text input if "
@@ -102,6 +112,7 @@ class ConfigurationProperty(Draft07JsonSchema):
 
     @validator("type", pre=True)
     def _coerce_type_name(cls, v):
+        """ceorces python type names to json schema type names."""
         if isinstance(v, type):
             v = v.__name__
         v = str(v).lower()
