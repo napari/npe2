5f1aebc7aca89a330821093a4eb86ade578bb8f5
Talley Lambert <talley.lambert@gmail.com>
2022-08-16 14:26:02 -0400

    fix: relax display name

--- npe2/manifest/schema.py
+++ npe2/manifest/schema.py
@@ -56,10 +56,10 @@ class PluginManifest(ImportExportModel):
     display_name: str = Field(
         "",
         description="User-facing text to display as the name of this plugin. "
-        "Must be 3-40 characters long, containing printable word characters, "
-        "and must not begin or end with an underscore, white space, or "
-        "non-word character. If not provided, the manifest `name` will be used as the "
-        "display name.",
+        "Must be 3-90 characters long. If not provided, the manifest `name` will be "
+        "used as the display name.",
+        min_length=3,
+        max_length=90,
     )
 
     _validate_display_name = validator("display_name", allow_reuse=True)(
