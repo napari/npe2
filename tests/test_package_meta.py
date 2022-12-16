7261210e463958721fe218d8131235ea3a8c1f1c
Ashley Anderson <aganders3@gmail.com>
2022-12-15 22:04:20 -0500

    Fix PackageMetadata validation error with extra provided field (#256)
    * Fix PackageMetadata validation error with extra provided key (should be ignored)
    
    * Rename key -> field
    
    * Add pyside6 to tests
    
    * Revert "Add pyside6 to tests"
    
    This reverts commit cf639c23790d4ea22dc2b44e5eaf9c4ff78d56e3.
    
    Co-authored-by: Gonzalo Pena-Castellanos <goanpeca@gmail.com>
    Co-authored-by: Nathan Clack <nclack@chanzuckerberg.com>

--- tests/test_package_meta.py
+++ tests/test_package_meta.py
@@ -17,3 +17,24 @@ def test_package_metadata_version():
 
 def test_hashable():
     hash(PackageMetadata(name="test", version="1.0"))
+
+
+def test_package_metadata_extra_field():
+    pkg = {
+        "name": "test",
+        "version": "1.0",
+        "maintainer": "bob",
+        "extra_field_that_is_definitely_not_in_the_model": False,
+    }
+
+    try:
+        p = PackageMetadata(**pkg)
+    except Exception:
+        raise AssertionError(
+            "failed to parse PackageMetadata from a dict with an extra field"
+        )
+
+    assert p.name == "test"
+    assert p.version == "1.0"
+    assert p.maintainer == "bob"
+    assert not hasattr(p, "extra_field_that_is_definitely_not_in_the_model")
