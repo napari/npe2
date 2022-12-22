53fa836a07de0523d41cfe9bc54724bcf6c261c3
Kira Evans <contact@kne42.me>
2022-12-22 15:34:41 -0800

    address pr comments

--- npe2/manifest/contributions/_keybindings.py
+++ npe2/manifest/contributions/_keybindings.py
@@ -5,7 +5,6 @@ from pydantic.fields import Field
 from ..utils import Executable
 
 
-# TODO: uncomment code when corresponding features enabled in napari
 class KeyBindingContribution(Executable):
     command: str = Field(
         description="Identifier of the command to run when keybinding is triggered."
@@ -13,10 +12,10 @@ class KeyBindingContribution(Executable):
     # the existence of the command is not validated at registration-time,
     # but rather at call time... (since commands from other extensions can be called)
     key: str = Field(
-        description="Key or key sequence (separate keys with plus-sign and sequences "
-        "with space, e.g. Ctrl+O"  # and Ctrl+L L for a chord)."
+        description="Key or key sequence (separate simultaneous key presses with "
+        "a plus-sign e.g. Ctrl+O and sequences with a space e.g. Ctrl+L L for a chord)."
     )
     mac: Optional[str] = Field(description="Mac specific key or key sequence.")
     linux: Optional[str] = Field(description="Linux specific key or key sequence.")
     win: Optional[str] = Field(description="Windows specific key or key sequence.")
-    # when: Optional[str] = Field(description="Condition when the key is active.")
+    when: Optional[str] = Field(description="Condition when the key is active.")
