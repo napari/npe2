2f552ae1fd213d6b63961fda43574024edf754f8
Grzegorz Bokota <bokota+github@gmail.com>
2023-11-21 20:58:40 +0100

    add constraint for brainreg-segment in test

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -46,7 +46,7 @@ def test_fetch_npe1_manifest_dock_widget_as_attribute():
     # napari_experimental_provide_dock_widget parser, (where the return value
     # is a dotted attribute, rather than a direct name).  I only saw it in
     # brainreg-segment.
-    mf = fetch_manifest("brainreg-segment")
+    mf = fetch_manifest("brainreg-segment", version="0.2.18")
     assert mf.name == "brainreg-segment"
     assert mf.contributions.widgets
     # Test will eventually fail when brainreg-segment is updated to npe2
