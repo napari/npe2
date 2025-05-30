a257fa5408d334fd43ba4a6568e398f089f2a8d2
Carol Willing <carolcode@willingconsulting.com>
2025-05-30 12:32:12 -0700

    Add filter warnings for deprecations and comment out npe1 plugin on test_fetch_urls

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -65,7 +65,7 @@ def test_from_pypi_wheel_bdist_missing():
             fetch_manifest("my-package")
 
 
-@pytest.mark.skipif(not os.getenv("CI"), reason="slow, only run on CI")
+@pytest.mark.filterwarnings("ignore:Python 3.14 will, by default, filter extracted tar archives")
 def test_manifest_from_sdist():
     mf = _manifest_from_pypi_sdist("zarpaint")
     assert mf.name == "zarpaint"
@@ -84,7 +84,7 @@ def test_get_hub_plugin():
     assert info["name"] == "napari-svg"
 
 
-@pytest.mark.skipif(not os.getenv("CI"), reason="slow, only run on CI")
+@pytest.mark.filterwarnings("ignore:Python 3.14 will, by default, filter extracted tar archives")
 @pytest.mark.parametrize(
     "url",
     [
@@ -92,10 +92,7 @@ def test_get_hub_plugin():
         "https://files.pythonhosted.org/packages/5d/ae/17779e12ce60d8329306963e1a8dec608465caee582440011ff0c1310715/example_plugin-0.0.7-py3-none-any.whl",
         "git+https://github.com/napari/dummy-test-plugin.git@npe1",
         # this one doesn't use setuptools_scm, can check direct zip without clone
-        # FIXME: skipping this test as it is failing due to a setuptools update
-        # see https://github.com/napari/npe2/issues/378
-        # we should update this test to use a different plugin
-        # "https://github.com/jo-mueller/napari-stl-exporter/archive/refs/heads/main.zip",
+        "https://github.com/jo-mueller/napari-stl-exporter/archive/refs/heads/main.zip",
     ],
 )
 def test_fetch_urls(url):
