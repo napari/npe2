84982cc0b4e0a4f43de8a6d697a24dec2ed90541
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2025-07-02 09:27:34 -0400

    add description to the Fields for menus and submenus

--- src/npe2/manifest/contributions/_contributions.py
+++ src/npe2/manifest/contributions/_contributions.py
@@ -36,8 +36,8 @@ class ContributionPoints(BaseModel):
     widgets: Optional[List[WidgetContribution]]
     sample_data: Optional[List[SampleDataContribution]]
     themes: Optional[List[ThemeContribution]]
-    menus: Dict[str, List[MenuItem]] = Field(default_factory=dict, hide_docs=False)
-    submenus: Optional[List[SubmenuContribution]] = Field(None, hide_docs=False)
+    menus: Dict[str, List[MenuItem]] = Field(default_factory=dict, hide_docs=False, description="Menus contributed by the plugin.")
+    submenus: Optional[List[SubmenuContribution]] = Field(None, hide_docs=False, description="Submenus contributed by the plugin.")
     keybindings: Optional[List[KeyBindingContribution]] = Field(None, hide_docs=True)
 
     configuration: List[ConfigurationContribution] = Field(
