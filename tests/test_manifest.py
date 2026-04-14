ceed366eadeedc686873511d1e0f9d42602d2113
Lorenzo Gaifas <brisvag@gmail.com>
2026-04-14 09:55:05 +0200

    add test

--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -182,7 +182,18 @@ def test_visibility():
 
 
 def test_icon():
-    PluginManifest(name="myplugin", icon="my_plugin:myicon.png")
+    pm = PluginManifest(name="myplugin", icon="my_plugin:myicon.png")
+    assert pm.icon == "my_plugin:myicon.png"
+    pm = PluginManifest(name="myplugin", icon="https://example.com/icon.png")
+    assert pm.icon == "https://example.com/icon.png"
+    with pytest.raises(ValueError, match="not a valid icon URL"):
+        pm = PluginManifest(name="myplugin", icon="http://example.com/bad_icon.png")
+    pm = PluginManifest(
+        name="myplugin",
+        icon={"dark": "my_plugin:myicon.png", "light": "https://example.com/icon.png"},
+    )
+    assert pm.icon.dark == "my_plugin:myicon.png"
+    assert pm.icon.light == "https://example.com/icon.png"
 
 
 def test_dotted_plugin_name():
