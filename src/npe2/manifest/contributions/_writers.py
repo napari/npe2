9d671e34e1a7cdbb47bec94445c00d44ad86d81c
Lorenzo Gaifas <brisvag@gmail.com>
2026-07-20 12:28:44 +0200

    fix prek 2

--- src/npe2/manifest/contributions/_writers.py
+++ src/npe2/manifest/contributions/_writers.py
@@ -1,11 +1,11 @@
-from enum import Enum
+from enum import StrEnum
 
 from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 from npe2.manifest.utils import Executable
 
 
-class LayerType(str, Enum):
+class LayerType(StrEnum):
     graph = "graph"
     image = "image"
     labels = "labels"
